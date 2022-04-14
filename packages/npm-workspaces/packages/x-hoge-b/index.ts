e1a954bb9bb4935c4d4d8ca47328d14df2942ac1
maroon8021 <okihiutat.iutim@gmail.com>
2022-04-14 18:49:59 +0900

    Added npm workspaces

---
+++ packages/npm-workspaces/packages/x-hoge-b/index.ts
@@ -0,0 +1,1 @@
+export const calc2 = (a: number, b: number) => a + b;
---
+++ packages/npm-workspaces/src/main/index.ts
@@ -0,0 +1,11 @@
+import { calc } from "x-hoge-a";
+import { calc2 } from "x-hoge-b";
+
+const run = () => {
+  const num = calc(1, 2);
+  const num2 = calc2(num, 3);
+  console.log("hoge");
+  console.log(num2);
+};
+
+run();
