780fb12d5ff12253a6f69750b0f4b20f4368230f
maroon8021 <okihiutat.iutim@gmail.com>
2022-12-06 20:43:49 +0900

    update

--- SUBURIs/cookie/backend/src/index.ts
+++ SUBURIs/cookie/backend/src/index.ts
@@ -7,12 +7,12 @@ const PORT = 50000;
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000" }));
 
-app.all("/*", async (_req: Request, res: Response) => {
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  return res.status(200).send({
-    message: "Hello World!!!",
-  });
-});
+// app.all("/*", async (_req: Request, res: Response) => {
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   return res.status(200).send({
+//     message: "Hello World!!!",
+//   });
+// });
 
 app.get("/hello", async (_req: Request, res: Response) => {
   return res.status(200).send({
@@ -28,6 +28,7 @@ app.post("/set-only-same-origin", async (_req: Request, res: Response) => {
 });
 
 app.post("/set-any", async (_req: Request, res: Response) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.cookie("any", "true", {
     domain: "localhost",
     path: "/",
@@ -42,6 +43,7 @@ app.post("/set-any", async (_req: Request, res: Response) => {
 
 app.get("/check-cookie", async (req: Request, res: Response) => {
   console.log(req.cookies);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   return res.status(200).send({
     message: "hoge",
   });
